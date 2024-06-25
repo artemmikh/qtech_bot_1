e48e615585954a9cacce6f8ad136932a8f7ac3ac
artemmikh <artem.mikh100@gmail.com>
2024-06-25 11:19:03 +0300

    правки по ревью

--- bot/main.py
+++ bot/main.py
@@ -4,22 +4,22 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 from dotenv import load_dotenv
 
+from const import NEW_EMPLOYEE, OLD_EMPLOYEE, MOSCOW_NO, MOSCOW_YES
+
 load_dotenv()
 
 TOKEN = os.getenv('BOT_TOKEN')
 
 
-# методы, обрабатывающие обновления (например, команды от пользователя),
-# принимают два обязательных аргумента: update и context.
-def start(update, context):
+def start_handler(update, context):
     """Обработчик команды /start"""
     keyboard = [
         [InlineKeyboardButton(
             'Я новый сотрудник',
-            callback_data='new_employee')],
+            callback_data=NEW_EMPLOYEE)],
         [InlineKeyboardButton(
             'Я работаю здесь уже долгое время',
-            callback_data='old_employee')]
+            callback_data=OLD_EMPLOYEE)]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text(
@@ -27,17 +27,17 @@ def start(update, context):
         reply_markup=reply_markup)
 
 
-def moscow_office(update, context):
+def moscow_office_handler(update, context):
     """Обработчик кнопок про Москву"""
     query = update.callback_query
     query.answer()
 
-    if query.data == 'new_employee':
+    if query.data == NEW_EMPLOYEE:
         text = ('Добро пожаловать в ГК QTECH!! Этот чат-бот поможет '
                 'сориентироваться в первые дни работы '
                 'и узнать больше о нашей компании. '
                 'Посещаете ли вы офис в Москве?')
-    elif query.data == 'old_employee':
+    elif query.data == OLD_EMPLOYEE:
         text = ('Здорово, что вы присоединились к чат-боту! '
                 'Он поможет вам структурировать  информацию '
                 'о нашей компании воедино или узнать что-то новое. '
@@ -47,28 +47,28 @@ def moscow_office(update, context):
 
     keyboard = [
         [
-            InlineKeyboardButton('Да', callback_data='moscow_yes'),
-            InlineKeyboardButton('Нет', callback_data='moscow_no'),
+            InlineKeyboardButton('Да', callback_data=MOSCOW_YES),
+            InlineKeyboardButton('Нет', callback_data=MOSCOW_NO),
         ]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     query.edit_message_text(text=text, reply_markup=reply_markup)
 
 
-def info_buttons(update, context):
+def info_buttons_handler(update, context):
     """Обработчик нажатия кнопок"""
     query = update.callback_query
     query.answer()
 
-    if query.data == 'moscow_yes':
+    if query.data == MOSCOW_YES:
         keyboard = [
-            [  # на будущее - тут можно подгружать кнопки из базы
+            [
                 InlineKeyboardButton(
                     'Кнопка посещающих Москву',
                     callback_data='moscow_button')
             ]
         ]
-    elif query.data == 'moscow_no':
+    elif query.data == MOSCOW_NO:
         keyboard = [
             [
                 InlineKeyboardButton(
@@ -89,20 +89,13 @@ def main():
     """Основная функция для запуска бота"""
     updater = Updater(TOKEN, use_context=True)
     dispatcher = updater.dispatcher
-
-    # Обработчик команды /start
-    dispatcher.add_handler(CommandHandler('start', start))
-
-    # Обработчик для кнопок выбора нового или старого сотрудника.
-    # Pattern нужен чтобы Обработчик реагировал только на определённые запросы
+    dispatcher.add_handler(CommandHandler('start', start_handler))
     dispatcher.add_handler(CallbackQueryHandler(
-        moscow_office,
+        moscow_office_handler(),
         pattern='^(new_employee|old_employee)$'))
-
-    # Обработчик для кнопок ответа о посещении офиса в Москве
     dispatcher.add_handler(
-        CallbackQueryHandler(info_buttons, pattern='^(moscow_yes|moscow_no)$'))
-
+        CallbackQueryHandler(info_buttons_handler(),
+                             pattern='^(moscow_yes|moscow_no)$'))
     updater.start_polling()
     updater.idle()
 
