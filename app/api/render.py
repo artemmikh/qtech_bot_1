5473de1a2c76311a8e30af62484b99e887bc3ad5
Pavel Razgunosov <razgunosov@yandex.ru>
2024-07-11 11:46:51 +0300

    Validation for update

--- app/api/render.py
+++ app/api/render.py
@@ -45,7 +45,7 @@ async def post_button_form(
         request: Request,
         name: str = Form(...),
         is_moscow: bool = Form(...),
-        text: str = Form(None),
+        text: str = Form(''),
         is_department: bool = Form(...),
         is_active: bool = Form(...),
         file_pic: list[UploadFile] = None,
@@ -138,41 +138,52 @@ async def update_button_form(request: Request,
 
 @router.post("/update/{button_id}")
 async def update_button_form(
+        request: Request,
         button_id: int,
         name: str = Form(...),
         is_moscow: bool = Form(...),
-        text: str = Form(...),
+        text: str = Form(''),
         is_department: bool = Form(...),
         is_active: bool = Form(...),
-        file_pic: list[UploadFile] = File(...),
-        file_doc: list[UploadFile] = File(...),
+        file_pic: list[UploadFile] = None,
+        file_doc: list[UploadFile] = None,
         session: AsyncSession = Depends(get_async_session),
 ):
-    button = await get_button_detail_by_id(button_id, session)
-    button.name = name
-    button.is_moscow = is_moscow
-    button.text = text
-    button.is_department = is_department
-    button.is_active = is_active
+    form = ButtonForm(request)
+    await form.load_data()
+    if await form.is_valid():
+        button = await get_button_detail_by_id(button_id, session)
+        button.name = name
+        button.is_moscow = is_moscow
+        button.text = text
+        button.is_department = is_department
+        button.is_active = is_active
 
-    if file_pic[0].filename != '':
-        picture = ' '.join(object_upload(settings.PICTURE_ROOT, settings.BASE_DIR, file_pic))
-        if button.picture:
-            button.picture = f'{str(button.picture)} {picture}'
-        else:
-            button.picture = picture
+        if file_pic[0].filename != '':
+            picture = ' '.join(object_upload(settings.PICTURE_ROOT, settings.BASE_DIR, file_pic))
+            if button.picture:
+                button.picture = f'{str(button.picture)} {picture}'
+            else:
+                button.picture = picture
 
-    if file_doc[0].filename != '':
-        file = ' '.join(object_upload(settings.DOC_ROOT, settings.BASE_DIR, file_doc))
-        if button.file:
-            button.file = f'{str(button.file)} {file}'
-        else:
-            button.file = file
+        if file_doc[0].filename != '':
+            file = ' '.join(object_upload(settings.DOC_ROOT, settings.BASE_DIR, file_doc))
+            if button.file:
+                button.file = f'{str(button.file)} {file}'
+            else:
+                button.file = file
 
-    await session.commit()
-    return RedirectResponse(router.url_path_for('get_button_detail', button_id=button_id),
-                            status_code=status.HTTP_303_SEE_OTHER)
-
+        await session.commit()
+        return RedirectResponse(router.url_path_for('render_all_buttons'), status_code=status.HTTP_303_SEE_OTHER)
+    else:
+        errors = form.errors
+        return templates.TemplateResponse("form_patch.html", {"request": request,
+                                                        "errors": errors,
+                                                        "context": form,
+                                                        'button_id': button_id
+                                                        }
+                                          )
+        
 
 @router.post(
     '/delete_picture/{button_id}/{picture}',
