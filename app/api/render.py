c21875dd6f7354efec7d3af9c6f603ec56adb5f7
artemmikh <artem.mikh100@gmail.com>
2024-07-15 10:52:55 +0300

    Исправление ошибки рендера страницы /create

--- app/api/render.py
+++ app/api/render.py
@@ -44,12 +44,11 @@ async def render_all_buttons(
 
 @router.get("/create", response_class=HTMLResponse)
 async def get_button_form(request: Request,
-                          user: User = Depends(get_current_user_from_token)):
-    context = {
-        "request": request,
-        "user": user
-    }
-    return templates.TemplateResponse("form.html", context)
+                          user: User = Depends(get_current_user_from_token)
+                          ):
+    return templates.TemplateResponse("form.html", {"request": request,
+                                                    "context": None,
+                                                    'user': user})
 
 
 @router.post("/create")
@@ -65,7 +64,6 @@ async def post_button_form(
 
         session: AsyncSession = Depends(get_async_session),
 ):
-
     form = ButtonForm(request)
     await form.load_data()
     if await form.is_valid():
