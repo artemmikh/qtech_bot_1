e7b70cb9ef71e441596c53dbc7d5787becea14b3
Михайлов Артём <artem.mikh100@gmail.com>
2024-10-23 18:49:11 +0300

    Добавление корректного json для swagger

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -6,8 +6,26 @@ class UserRead(schemas.BaseUser[int]):
 
 
 class UserCreate(schemas.BaseUserCreate):
-    pass
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "user@example.com",
+                "password": "12345678",  # Здесь указываем новый пароль
+                "is_active": True,
+                "is_superuser": False,
+                "is_verified": False
+            }
+        }
 
 
 class UserUpdate(schemas.BaseUserUpdate):
-    pass
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "user@example.com",
+                "password": "123456789",  # Пример нового пароля при обновлении
+                "is_active": True,
+                "is_superuser": False,
+                "is_verified": False
+            }
+        }
